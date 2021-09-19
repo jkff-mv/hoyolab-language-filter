e2ee39601fd9b21233ff4506039da56cb2c27370
JK-FF <jkff.maidenvoyage@gmail.com>
2021-09-19 18:58:21 +0900

    フォーマッタを適用。

--- hoyolab/content2.js
+++ hoyolab/content2.js
@@ -6,64 +6,64 @@ var isJapanese = false;
 
 
 function includeJa(text) {
-        try {
-            let gmi = 'gmi';
-            let regeIncludeHiragana = '^(?=.*[\u3041-\u3096]).*$';
-            let regeIncludeKatakana = '^(?=.*[\u30A1-\u30FA]).*$';
-            let regeIncludeKanji = '^(?=.*[\u4E00-\u9FFF]).*$';
-            let regeHiragana = new RegExp(regeIncludeHiragana, gmi);
-            let regeKatakana = new RegExp(regeIncludeKatakana, gmi);
-            let regeKanji = new RegExp(regeIncludeKanji, gmi);
+  try {
+    let gmi = 'gmi';
+    let regeIncludeHiragana = '^(?=.*[\u3041-\u3096]).*$';
+    let regeIncludeKatakana = '^(?=.*[\u30A1-\u30FA]).*$';
+    let regeIncludeKanji = '^(?=.*[\u4E00-\u9FFF]).*$';
+    let regeHiragana = new RegExp(regeIncludeHiragana, gmi);
+    let regeKatakana = new RegExp(regeIncludeKatakana, gmi);
+    let regeKanji = new RegExp(regeIncludeKanji, gmi);
 
-            let includeJa = false;
-            if (regeHiragana.test(text))
-                includeJa = true;
-            if (regeKatakana.test(text))
-                includeJa = true;
-            if (regeKanji.test(text))
-                includeJa = true;
+    let includeJa = false;
+    if (regeHiragana.test(text))
+      includeJa = true;
+    if (regeKatakana.test(text))
+      includeJa = true;
+    if (regeKanji.test(text))
+      includeJa = true;
 
-            return includeJa;
-        } catch (error) {
-            alert(error);
-        }
-    }
+    return includeJa;
+  } catch (error) {
+    alert(error);
+  }
+}
 
 // MO instance
 const mutationObserver = new MutationObserver(callback);
 // callback
-function callback(mutations){
+function callback(mutations) {
   //console.log(mutations);
   //mutations.forEach( mutation => {
-    // console.log(mutation);
-    let element = document.getElementsByClassName("mhy-article-card");
-    console.log(element.length);
-    for(let i = 0; i < element.length; i++){
-      console.log(i);
-      //console.log(element[i]);
-      titlehead = element[i].getElementsByClassName("mhy-article-card__title");
-      console.log(titlehead);
-      title = titlehead[0].innerText;
-/*
-      for(var j=0; j < title.length; j++){
-        if(title.charCodeAt(j) >= 256) {
-          isJapanese = true;
-        break;
-      }else{
-        isJapanese = false;
-      }
-      }
-      */
-      isJapanese = includeJa(title);
-      console.log(title);
-      console.log(isJapanese);
+  // console.log(mutation);
+  let element = document.getElementsByClassName("mhy-article-card");
+  console.log(element.length);
+  for (let i = 0; i < element.length; i++) {
+    console.log(i);
+    //console.log(element[i]);
+    titlehead = element[i].getElementsByClassName("mhy-article-card__title");
+    console.log(titlehead);
+    title = titlehead[0].innerText;
+    /*
+          for(var j=0; j < title.length; j++){
+            if(title.charCodeAt(j) >= 256) {
+              isJapanese = true;
+            break;
+          }else{
+            isJapanese = false;
+          }
+          }
+          */
+    isJapanese = includeJa(title);
+    console.log(title);
+    console.log(isJapanese);
 
-      if(isJapanese == false){
-        element[i].remove();
-      }
-
+    if (isJapanese == false) {
+      element[i].remove();
     }
-;
+
+  }
+  ;
 }
 
 // options
